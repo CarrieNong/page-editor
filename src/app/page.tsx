f614ff1d91a96ed7fb2f2a015af551ebb844fd11
CarrieNong <carrienon@carrienondeMacBook-Pro.local>
2024-11-05 11:27:50 +0800

    relayout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,17 +11,25 @@ export default function Home() {
   return (
     <div>
       <Layout className="overflow-hidden w-full h-dvh ">
-        <Header className="h-16 bg-white border-solid border-b border-slate-200">
-          Header
-        </Header>
+        <Sider
+          width="5%"
+          className="bg-white border-solid border-r border-slate-200"
+        >
+          Sider
+        </Sider>
         <Layout>
-          <Sider
-            width="5%"
-            className="bg-white border-solid border-r border-slate-200"
-          >
-            Sider
-          </Sider>
-          <Content style={contentStyle}>Content</Content>
+          <Header className="h-16 bg-white border-solid border-b border-slate-200">
+            Header
+          </Header>
+          <Layout style={contentStyle}>
+            <Sider width="20%" className="bg-white">
+              Sider
+            </Sider>
+            <Content>Content</Content>
+            <Sider width="25%" className="bg-white">
+              Sider
+            </Sider>
+          </Layout>
         </Layout>
       </Layout>
     </div>

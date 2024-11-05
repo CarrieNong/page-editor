1f3b0527323608aa952d913f8042405ff5da2153
CarrieNong <carrienon@carrienondeMacBook-Pro.local>
2024-11-05 12:14:49 +0800

    add style of header and sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 import { Layout } from "antd"
 const { Header, Sider, Content } = Layout
+import { BgColorsOutlined } from "@ant-design/icons"
+import HeaderTop from "@/components/HeaderTop"
 
 const contentStyle: React.CSSProperties = {
   height: "calc(100vh-64)",
@@ -13,13 +15,18 @@ export default function Home() {
       <Layout className="overflow-hidden w-full h-dvh ">
         <Sider
           width="5%"
-          className="bg-white border-solid border-r border-slate-200"
+          className="bg-white border-solid border-r border-slate-200 text-center"
+          style={{ color: "#1677ff" }}
         >
-          Sider
+          <BgColorsOutlined
+            className="mt-16 mb-1"
+            style={{ fontSize: "30px" }}
+          />
+          <p className="text-sm font-bold">装修</p>
         </Sider>
         <Layout>
           <Header className="h-16 bg-white border-solid border-b border-slate-200">
-            Header
+            <HeaderTop />
           </Header>
           <Layout style={contentStyle}>
             <Sider width="20%" className="bg-white">
---
+++ src/components/HeaderTop/index.jsx
@@ -0,0 +1,15 @@
+import { Flex, Button } from "antd"
+
+const HeaderTop = () => {
+  return (
+    <Flex className="h-16" gap="small" align="center" justify="flex-end">
+      <Button type="primary">发布</Button>
+      <Button>预览</Button>
+      <Button type="primary" danger>
+        重置
+      </Button>
+    </Flex>
+  )
+}
+
+export default HeaderTop

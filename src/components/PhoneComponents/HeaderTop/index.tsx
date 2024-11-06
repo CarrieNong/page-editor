0e80cb843de4169c58b448690d63e5ff18e69018
CarrieNong <carrienon@carrienondeMacBook-Pro.local>
2024-11-06 15:51:04 +0800

    phone header

---
+++ src/components/PhoneComponents/HeaderTop/index.tsx
@@ -0,0 +1,14 @@
+import { LeftOutlined } from "@ant-design/icons"
+
+const HeaderTop = () => {
+  return (
+    <section className="w-full h-9 flex justify-center items-center relative bg-white">
+      <div className="absolute top-2 left-1">
+        <LeftOutlined className="text-xl" />
+      </div>
+      <div>页面标题</div>
+    </section>
+  )
+}
+
+export default HeaderTop
--- src/components/PhonePage/index.tsx
+++ src/components/PhonePage/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import topUrl from "@/app/assets/images/phoneTop.png"
+import HeaderTop from "../PhoneComponents/HeaderTop"
 import "./index.css"
 
 const PhonePage = () => {
@@ -7,6 +8,7 @@ const PhonePage = () => {
     <div className="w-1/2 h-full overflow-y-scroll m-auto content">
       <div className="my-11 mx-auto bg-slate-50 shadow-md phone-wrap">
         <Image src={topUrl} alt="phone" />
+        <HeaderTop />
       </div>
     </div>
   )

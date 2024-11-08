aa36c7b8f2297122f8304e0cb341f45ac1a8eb5d
CarrieNong <carrienon@carrienondeMacBook-Pro.local>
2024-11-08 14:54:58 +0800

    add bar com

---
+++ src/components/PhoneComponents/TabCom/index.tsx
@@ -0,0 +1,46 @@
+import { Badge, TabBar } from "antd-mobile"
+import {
+  AppOutline,
+  MessageOutline,
+  UnorderedListOutline,
+  UserOutline,
+} from "antd-mobile-icons"
+
+const tabs = [
+  {
+    key: "home",
+    title: "首页",
+    icon: <AppOutline />,
+    badge: Badge.dot,
+  },
+  {
+    key: "todo",
+    title: "待办",
+    icon: <UnorderedListOutline />,
+    badge: "5",
+  },
+  {
+    key: "message",
+    title: "消息",
+    icon: <MessageOutline />,
+  },
+  {
+    key: "personalCenter",
+    title: "我的",
+    icon: <UserOutline />,
+  },
+]
+
+const TabCom = () => {
+  return (
+    <section className="absolute bottom-0 left-0 w-full bg-white">
+      <TabBar>
+        {tabs.map((item) => (
+          <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+        ))}
+      </TabBar>
+    </section>
+  )
+}
+
+export default TabCom
--- src/components/PhonePage/index.tsx
+++ src/components/PhonePage/index.tsx
@@ -3,16 +3,18 @@ import topUrl from "@/app/assets/images/phoneTop.png"
 import HeaderCom from "../PhoneComponents/HeaderCom"
 import SearchCom from "../PhoneComponents/SearchCom"
 import TitleCom from "../PhoneComponents/TitleCom"
+import TabCom from "../PhoneComponents/TabCom"
 import "./index.css"
 
 const PhonePage = () => {
   return (
     <div className="w-1/2 h-full overflow-y-scroll m-auto content">
-      <div className="my-11 mx-auto bg-slate-50 shadow-md phone-wrap">
+      <div className="my-11 mx-auto bg-slate-50 shadow-md phone-wrap relative">
         <Image src={topUrl} alt="phone" />
         <HeaderCom />
         <SearchCom />
         <TitleCom />
+        <TabCom />
       </div>
     </div>
   )

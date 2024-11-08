de1938c451890527cdbf1138e40215864c21fa7e
CarrieNong <carrienon@carrienondeMacBook-Pro.local>
2024-11-08 14:42:25 +0800

    modify import lib

--- src/components/PhoneComponents/HeaderCom/index.tsx
+++ src/components/PhoneComponents/HeaderCom/index.tsx
@@ -1,10 +1,10 @@
-import { LeftOutlined } from "@ant-design/icons"
+import { LeftOutline } from "antd-mobile-icons"
 
 const HeaderCom = () => {
   return (
     <section className="w-full h-9 flex justify-center items-center relative bg-white">
       <div className="absolute top-2 left-1">
-        <LeftOutlined className="text-xl" />
+        <LeftOutline className="text-xl" />
       </div>
       <div>页面标题</div>
     </section>

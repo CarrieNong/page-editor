92111a2279ff5354e17ed0dc5cd8c11c012c41f8
CarrieNong <carrienon@carrienondeMacBook-Pro.local>
2024-11-15 11:40:53 +0800

    component initial data

--- src/components/ComponentsList/index.tsx
+++ src/components/ComponentsList/index.tsx
@@ -11,22 +11,61 @@ const componentsList = [
   {
     text: "商品搜索",
     icon: <SearchOutlined style={{ fontSize: "32px" }} />,
-    name: "GoodSearch",
+    name: "SearchCom",
+    data: {
+      comName: "SearchCom",
+      hotWord: "搜索商品",
+      wordPosition: "center",
+      bgColor: "#fff",
+    },
   },
   {
     text: "标题文本",
     icon: <FontSizeOutlined style={{ fontSize: "32px" }} />,
-    name: "TitleText",
+    name: "TitleCom",
+    data: {
+      comName: "TitleCom",
+      title: "标题文本",
+      desc: "描述内容描述内容描述内容描述内容",
+      titleSize: 16,
+      descSize: 12,
+    },
   },
   {
     text: "底部导航",
     icon: <SendOutlined style={{ fontSize: "32px" }} />,
-    name: "BottomNavigation",
+    name: "TabCom",
+    data: {
+      comName: "TabCom",
+      chooseColor: "#1677ff",
+      noChooseColor: "#fff",
+      tabList: [
+        {
+          activeIcon: "",
+          noActiveIcon: "",
+          name: "",
+          url: "",
+        },
+      ],
+    },
   },
   {
     text: "商品",
     icon: <ShoppingOutlined style={{ fontSize: "32px" }} />,
-    name: "GoodList",
+    name: "ProductCom",
+    data: {
+      comName: "ProductCom",
+      mode: "",
+      themeColor: "#1677ff",
+      productList: [
+        {
+          id: "",
+          price: "",
+          name: "",
+          desc: "",
+        },
+      ],
+    },
   },
 ]
 

057cee0dec6f31818d0c204e18dba384088a6c36
CarrieNong <carrienon@carrienondeMacBook-Pro.local>
2024-11-18 15:37:36 +0800

    restrict tabcom number

--- src/components/DragScale/index.tsx
+++ src/components/DragScale/index.tsx
@@ -1,16 +1,29 @@
 "use client"
-import { Layout } from "antd"
+import { Layout, message } from "antd"
 const { Sider, Content } = Layout
 import ComponentsList from "@/components/ComponentsList"
 import PhonePage from "@/components/PhonePage"
 import { usePage, usePageDispatch } from "@/app/PageContext"
 import { DndContext } from "@dnd-kit/core"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 const DragScale = () => {
   const page = usePage()
   const dispatch = usePageDispatch()
   const [activeId, setActiveId] = useState(null)
+  const [messageApi, contextHolder] = message.useMessage()
+  const [showWarning, setShowWarning] = useState(false)
+
+  useEffect(() => {
+    if (showWarning) {
+      messageApi.open({
+        type: "warning",
+        content: "当前组件只能添加一个",
+      })
+      setShowWarning(false) // 防止重复触发
+    }
+  }, [showWarning, messageApi])
+
   function handleDragStart(event) {
     setActiveId(event.active.id)
   }
@@ -38,12 +51,26 @@ const DragScale = () => {
 
     if (over) {
       let componentList = page.componentList
-      componentList.push({ name: event.active.id })
-      componentList = page.componentList.filter((t) => t.name !== "AddCom")
-      dispatch({
-        type: "added",
-        componentList,
-      })
+      let comName = event.active.id
+      const tabCom = page.componentList.filter((t) => t.name == "TabCom")
+      const existTabCom = comName == "TabCom" && tabCom.length
+      componentList = componentList.filter((t) => t.name !== "AddCom")
+      if (existTabCom) {
+        setShowWarning(true) // 触发警告消息
+        dispatch({
+          type: "changed",
+          page: {
+            ...page,
+            componentList,
+          },
+        })
+      } else {
+        componentList.push({ name: comName })
+        dispatch({
+          type: "added",
+          componentList,
+        })
+      }
     }
   }
   return (
@@ -52,6 +79,7 @@ const DragScale = () => {
       onDragEnd={handleDragEnd}
       onDragOver={handleDragOver}
     >
+      {contextHolder}
       <Sider width="20%" className="bg-white">
         <ComponentsList activeId={activeId} />
       </Sider>
